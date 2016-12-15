6cb4a5a298bf441901a85e833c6d6903fe8f838f
Thomas Crines <thomascrines@hotmail.com>
2016-12-15 12:09:31 +0000

    added dummy server

--- server.js
+++ server.js
@@ -1,3 +1,16 @@
-Hello guys!
+var express = require('express');
+var app = express();
+var path = require('path');
 
-Hi chaps!
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.use(express.static('public'));
+
+var server = app.listen(3000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+});
