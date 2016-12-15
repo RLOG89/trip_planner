6cfa41712ee14ecbd0080b44bf69d5dc11f83b2d
Thomas Crines <thomascrines@hotmail.com>
2016-12-15 15:05:46 +0000

    whisky button developed with Adrian

--- client/build/bundle.js
+++ client/build/bundle.js
@@ -45,34 +45,64 @@
 /***/ function(module, exports, __webpack_require__) {
 
 	var whisky = __webpack_require__(1);
-	var sports = __webpack_require__(6);
-	var movies = __webpack_require__(7);
-	var historic = __webpack_require__(5);
+	var sports = __webpack_require__(2);
+	var movies = __webpack_require__(3);
+	var historic = __webpack_require__(4);
 	
 	
 	
 	var app = function() {
-	  populateList(whisky);
-	  populateList(sports);
-	  populateList(movies);
-	  populateList(historic);
-	}
+	  console.log("here");
+	  var whiskyButton = document.getElementById('distilleries');
+	  var whiskyActive = false;
+	  whiskyButton.onclick = function() {
+	    if (whiskyActive) {
+	      whiskyActive = false;
+	      populateList(whisky,true);
+	    } else {
+	      whiskyActive = true;
+	      populateList(whisky);
+	    }
+	  }
+	};
 	
-	var populateList = function(thing) {
+	  //   populateList(sports);
+	  //   populateList(movies);
+	  //   populateList(historic);
+	  // }
+	// var populateList = function(thing) {
+	//   var list = document.getElementById('list');
+	//   thing.forEach(function(item) {
+	//     var liName = document.createElement('li');
+	//     var liLocation = document.createElement('li');
+	//     var liDescription = document.createElement('li');
+	//     var liImg = document.createElement('img');
+	//     liName.innerText = item.name;
+	//     liLocation.innerText = item.location;
+	//     liDescription.innerText = item.description;
+	//     liImg.src = item.img;
+	//     list.appendChild(liName);
+	//     list.appendChild(liLocation);
+	//     list.appendChild(liDescription);
+	//     list.appendChild(liImg);
+	//   })
+	// };
+	
+	var populateList = function(thing,clear) {
 	  var list = document.getElementById('list');
+	  list.innerHTML = "";
+	  if (clear) return;
 	  thing.forEach(function(item) {
-	    var liName = document.createElement('li');
-	    var liLocation = document.createElement('li');
-	    var liDescription = document.createElement('li');
-	    var liImg = document.createElement('img');
-	    liName.innerText = item.name;
-	    liLocation.innerText = item.location;
-	    liDescription.innerText = item.description;
-	    liImg.src = item.img;
-	    list.appendChild(liName);
-	    list.appendChild(liLocation);
-	    list.appendChild(liDescription);
-	    list.appendChild(liImg);
+	    var destination = document.createElement('li');
+	    var checkbox = document.createElement('input');
+	    checkbox.type = 'checkbox';
+	    checkbox.name = item.name;
+	    checkbox.value = false;
+	    checkbox.id = item.name;
+	
+	    destination.innerText = item.name +', ' + item.location;
+	    list.appendChild(destination);
+	    list.appendChild(checkbox);
 	  })
 	};
 	
@@ -169,99 +199,7 @@
 
 
 /***/ },
-/* 2 */,
-/* 3 */,
-/* 4 */,
-/* 5 */
-/***/ function(module, exports) {
-
-	module.exports = [
-	  {
-	    "lat": 55.949099, 
-	    "lng": 3.199871,
-	    "name": "Edinburgh Castle",
-	    "location": "Edinburgh",
-	    "description": "Edinburgh Castle is a historic fortress which dominates the skyline of the city of Edinburgh, Scotland, from its position on the Castle Rock. Its importance as a part of Scotland's national heritage was recognised increasingly from the early 19th century onwards, and various restoration programmes have been carried out over the past century and a half. As one of the most important strongholds in the Kingdom of Scotland, Edinburgh Castle was involved in many historical conflicts from the Wars of Scottish Independence in the 14th century to the Jacobite Rising of 1745.",
-	    "img": "/images/edinburgh_castle.jpg",
-	  },
-	  {
-	    "lat": 59.0487,
-	    "lng": 3.3417,
-	    "name": "Skara Brae Prehistoric Village",
-	    "location": "Stromness",
-	    "description": "The Neolithic settlement of Skara Brae is one of the best preserved groups of prehistoric houses in Western Europe. Uncovered by a storm in 1850, Skara Brae gives a remarkable picture of life 5,000 years ago, before Stonehenge was built. Explore this prehistoric village and see ancient homes fitted with stone beds, dressers and seats. A replica house allows visitors to explore its interior, while the visitor centre provides touch-screen presentations, fact-finding quizzes and an opportunity to see artefacts discovered during the archaeological excavations of the 1970s.",
-	    "img": "/images/skara_brae.jpg"
-	  },
-	  {
-	    "lat": 57.4735,
-	    "lng": 4.0732,
-	    "name": "Clava Cairns",
-	    "location": "Inverness",
-	    "description": "Clava Cairns are a well-preserved Bronze Age cemetery complex of passage graves; ring cairns, kerb cairns and standing stones in a beautiful setting. Clava Cairns or the Prehistoric Burial Cairns of Bulnuaran of Clava are a group of three Bronze Age cairns located near Inverness. A hugely significant and exceptionally well preserved prehistoric site, Clava Cairns is a fantastic example of the distant history of Highland Scotland, dating back about 4,000 years.",
-	    "img": "/images/clava_cairns.jpg"
-	  },
-	  {
-	    "lat": 58.1975,
-	    "lng": 6.7452,
-	    "name": "Calanais Standing Stones",
-	    "location": "Stornoway",
-	    "description": "Walk among the megaliths at one of Scotland’s most magnificent and best-preserved Neolithic monuments. The Calanais Standing Stones are an extraordinary cross-shaped setting of stones erected 5,000 years ago. They predate England’s famous Stonehenge monument, and were an important place for ritual activity for at least 2,000 years.",
-	    "img": "/images/calanais.jpg"
-	  },
-	  {
-	    "lat": 55.9672, 
-	    "lng": 4.0670,
-	    "name": "Antoine Wall",
-	    "location": "Falkirk",
-	    "description": "The fascinating World Heritage Site of the Antonine Wall, once the most northern frontier of the Roman Empire in Britain, today offers great walking routes surrounded by nature. This three to four meters high stone and turf fortification stretches for nearly 60km across the Central Belt of Scotland between the towns of Bo’ness on the Firth of Forth and Old Kilpatrick on the Firth of Clyde, and is a popular site. Despite the passage of time, substantial lengths of the wall survived. The best preserved stretch of ditch, the 8km long Watling Lodge, is set in attractive countryside with oak woodland in Falkirk.",
-	    "img": "/images/antoine_wall.jpg"
-	  },
-	  {
-	    "lat": 56.5622, 
-	    "lng": 2.5824,
-	    "name": "Arbroath Abbey",
-	    "location": "Arbroath",
-	    "description": "Explore the substantial ruins of a Tironensian monastery, founded by William the Lion in 1178, who is buried in Arbroath Abbey. Arbroath Abbey is famously associated with the Declaration of Arbroath of 1320, which asserted Scotland's independence from England. Parts of the abbey church and domestic buildings remain, notably the gatehouse range and the abbot's house. Visitors can enjoy the herb garden along the south wall of the abbey church. The visitor centre provides a walk through audio-visual with a sound loop system. There is also a staff operated platform to enable disabled access to the upper viewing area.",
-	    "img": "/images/arbroath_abbey.jpg"
-	  },
-	  {
-	    "lat": 56.1334, 
-	    "lng": 5.4869,
-	    "name": "Kilmartin Glen",
-	    "location": "Kilmartin",
-	    "description": "Kilmartin Glen is located between Oban and Lochgilphead, surrounding the village of Kilmartin, on the west of Scotland. The area spans 5,000 years with a multitude of cairns, standing stones, carved rock, stone circles, forts and castles. Kilmartin Glen is considered to have one of the most important concentrations of Neolithic and Bronze Age remains in Scotland. There are more than 350 ancient monuments within a six mile radius of the village, with 150 of them being prehistoric. Monuments include standing stones, a henge monument, numerous cists, and a ‘linear cemetery’ comprising five burial cairns.",
-	    "img": "/images/kilmartin_glen.jpg"
-	  },
-	  {
-	    "lat": 55.8627, 
-	    "lng": 4.2311,
-	    "name": "The Necropolis",
-	    "location": "Glasgow",
-	    "description": "The Glasgow Necropolis is a Victorian cemetery in Glasgow, Scotland. It is on a low but very prominent hill to the east of Glasgow Cathedral (St. Mungo's Cathedral). Fifty thousand individuals have been buried here. Typically for the period only a small percentage are named on monuments and not every grave has a stone. Approximately 3500 monuments exist here.",
-	    "img": "/images/necropolis_glasgow.jpg"
-	  },
-	  {
-	    "lat": 58.9966, 
-	    "lng": 3.1882,
-	    "name": "Maeshowe Chambered Cairn",
-	    "location": "Stenness",
-	    "description": "Maeshowe is the finest chambered tomb in north west Europe and is more than 5,000 years old. Viking crusaders broke into the cairn in the 12th century and carved graffiti runes on the walls of the main chamber. Maeshowe is part of the Heart of Neolithic Orkney World Heritage Site, along with Skara Brae, the Ring of Brodgar and the Stones of Stenness. Admission to Maeshowe is through a timed ticket that gives a set time for the start of the guided tour. All visitors are required to book in at Tormiston Mill which is the visitor centre for Maeshowe. ",
-	    "img": "/images/maeshowe_chambered_cairn.jpg"
-	  },
-	  {
-	    "lat": 56.1238, 
-	    "lng": 3.9474,
-	    "name": "Stirling Castle",
-	    "location": "Stirling",
-	    "description": "Stirling Castle is one of Scotland's most historically important sites and was once a favoured residence of the Stewart kings and queens who held grand celebrations at the castle. Knights, nobles and foreign ambassadors once flocked to Stirling Castle to revel in its grandeur with its superb sculptures and beautiful gardens. It was a favoured residence of the Stewart kings and queens who held grand celebrations from christenings to coronations. Today you can meet the costumed characters in the roles of bodyguards, court officials, maids of honour and servants who will welcome you into 16th century life",
-	    "img": "/images/stirling_castle.jpg"
-	  }
-	
-	]
-
-
-/***/ },
-/* 6 */
+/* 2 */
 /***/ function(module, exports) {
 
 	module.exports = [
@@ -361,17 +299,17 @@
 
 
 /***/ },
-/* 7 */
+/* 3 */
 /***/ function(module, exports) {
 
 	 module.exports = [
-	{
-	    "name": "Braveheart",
-	    "location": "Fort William",
-	    "lat": 56.8198,
-	    "long": 5.1052,
-	    "description": "When his secret bride is executed for assaulting an English soldier who tried to rape her, William Wallace begins a revolt against King Edward I of England.",
-	    "img": "/images/braveheart.jpg"
+	 {
+	  "name": "Braveheart",
+	  "location": "Fort William",
+	  "lat": 56.8198,
+	  "long": 5.1052,
+	  "description": "When his secret bride is executed for assaulting an English soldier who tried to rape her, William Wallace begins a revolt against King Edward I of England.",
+	  "img": "/images/braveheart.jpg"
 	},
 	{
 	  "name": "Local Hero",
@@ -445,6 +383,95 @@
 	  "description": "A man in London tries to help a counterespionage agent. But when the agent is killed and the man stands accused, he must go on the run to both save himself and also stop a spy ring which is trying to steal top secret information",
 	  "img": "/images/39_steps.jpg"
 	}];
+
+/***/ },
+/* 4 */
+/***/ function(module, exports) {
+
+	module.exports = [
+	{
+	  "lat": 55.949099, 
+	  "lng": 3.199871,
+	  "name": "Edinburgh Castle",
+	  "location": "Edinburgh",
+	  "description": "Edinburgh Castle is a historic fortress which dominates the skyline of the city of Edinburgh, Scotland, from its position on the Castle Rock. Its importance as a part of Scotland's national heritage was recognised increasingly from the early 19th century onwards, and various restoration programmes have been carried out over the past century and a half. As one of the most important strongholds in the Kingdom of Scotland, Edinburgh Castle was involved in many historical conflicts from the Wars of Scottish Independence in the 14th century to the Jacobite Rising of 1745.",
+	  "img": "/images/edinburgh_castle.jpg",
+	},
+	{
+	  "lat": 59.0487,
+	  "lng": 3.3417,
+	  "name": "Skara Brae Prehistoric Village",
+	  "location": "Stromness",
+	  "description": "The Neolithic settlement of Skara Brae is one of the best preserved groups of prehistoric houses in Western Europe. Uncovered by a storm in 1850, Skara Brae gives a remarkable picture of life 5,000 years ago, before Stonehenge was built. Explore this prehistoric village and see ancient homes fitted with stone beds, dressers and seats. A replica house allows visitors to explore its interior, while the visitor centre provides touch-screen presentations, fact-finding quizzes and an opportunity to see artefacts discovered during the archaeological excavations of the 1970s.",
+	  "img": "/images/skara_brae.jpg"
+	},
+	{
+	  "lat": 57.4735,
+	  "lng": 4.0732,
+	  "name": "Clava Cairns",
+	  "location": "Inverness",
+	  "description": "Clava Cairns are a well-preserved Bronze Age cemetery complex of passage graves; ring cairns, kerb cairns and standing stones in a beautiful setting. Clava Cairns or the Prehistoric Burial Cairns of Bulnuaran of Clava are a group of three Bronze Age cairns located near Inverness. A hugely significant and exceptionally well preserved prehistoric site, Clava Cairns is a fantastic example of the distant history of Highland Scotland, dating back about 4,000 years.",
+	  "img": "/images/clava_cairns.jpg"
+	},
+	{
+	  "lat": 58.1975,
+	  "lng": 6.7452,
+	  "name": "Calanais Standing Stones",
+	  "location": "Stornoway",
+	  "description": "Walk among the megaliths at one of Scotland’s most magnificent and best-preserved Neolithic monuments. The Calanais Standing Stones are an extraordinary cross-shaped setting of stones erected 5,000 years ago. They predate England’s famous Stonehenge monument, and were an important place for ritual activity for at least 2,000 years.",
+	  "img": "/images/calanais.jpg"
+	},
+	{
+	  "lat": 55.9672, 
+	  "lng": 4.0670,
+	  "name": "Antoine Wall",
+	  "location": "Falkirk",
+	  "description": "The fascinating World Heritage Site of the Antonine Wall, once the most northern frontier of the Roman Empire in Britain, today offers great walking routes surrounded by nature. This three to four meters high stone and turf fortification stretches for nearly 60km across the Central Belt of Scotland between the towns of Bo’ness on the Firth of Forth and Old Kilpatrick on the Firth of Clyde, and is a popular site. Despite the passage of time, substantial lengths of the wall survived. The best preserved stretch of ditch, the 8km long Watling Lodge, is set in attractive countryside with oak woodland in Falkirk.",
+	  "img": "/images/antoine_wall.jpg"
+	},
+	{
+	  "lat": 56.5622, 
+	  "lng": 2.5824,
+	  "name": "Arbroath Abbey",
+	  "location": "Arbroath",
+	  "description": "Explore the substantial ruins of a Tironensian monastery, founded by William the Lion in 1178, who is buried in Arbroath Abbey. Arbroath Abbey is famously associated with the Declaration of Arbroath of 1320, which asserted Scotland's independence from England. Parts of the abbey church and domestic buildings remain, notably the gatehouse range and the abbot's house. Visitors can enjoy the herb garden along the south wall of the abbey church. The visitor centre provides a walk through audio-visual with a sound loop system. There is also a staff operated platform to enable disabled access to the upper viewing area.",
+	  "img": "/images/arbroath_abbey.jpg"
+	},
+	{
+	  "lat": 56.1334, 
+	  "lng": 5.4869,
+	  "name": "Kilmartin Glen",
+	  "location": "Kilmartin",
+	  "description": "Kilmartin Glen is located between Oban and Lochgilphead, surrounding the village of Kilmartin, on the west of Scotland. The area spans 5,000 years with a multitude of cairns, standing stones, carved rock, stone circles, forts and castles. Kilmartin Glen is considered to have one of the most important concentrations of Neolithic and Bronze Age remains in Scotland. There are more than 350 ancient monuments within a six mile radius of the village, with 150 of them being prehistoric. Monuments include standing stones, a henge monument, numerous cists, and a ‘linear cemetery’ comprising five burial cairns.",
+	  "img": "/images/kilmartin_glen.jpg"
+	},
+	{
+	  "lat": 55.8627, 
+	  "lng": 4.2311,
+	  "name": "The Necropolis",
+	  "location": "Glasgow",
+	  "description": "The Glasgow Necropolis is a Victorian cemetery in Glasgow, Scotland. It is on a low but very prominent hill to the east of Glasgow Cathedral (St. Mungo's Cathedral). Fifty thousand individuals have been buried here. Typically for the period only a small percentage are named on monuments and not every grave has a stone. Approximately 3500 monuments exist here.",
+	  "img": "/images/necropolis_glasgow.jpg"
+	},
+	{
+	  "lat": 58.9966, 
+	  "lng": 3.1882,
+	  "name": "Maeshowe Chambered Cairn",
+	  "location": "Stenness",
+	  "description": "Maeshowe is the finest chambered tomb in north west Europe and is more than 5,000 years old. Viking crusaders broke into the cairn in the 12th century and carved graffiti runes on the walls of the main chamber. Maeshowe is part of the Heart of Neolithic Orkney World Heritage Site, along with Skara Brae, the Ring of Brodgar and the Stones of Stenness. Admission to Maeshowe is through a timed ticket that gives a set time for the start of the guided tour. All visitors are required to book in at Tormiston Mill which is the visitor centre for Maeshowe. ",
+	  "img": "/images/maeshowe_chambered_cairn.jpg"
+	},
+	{
+	  "lat": 56.1238, 
+	  "lng": 3.9474,
+	  "name": "Stirling Castle",
+	  "location": "Stirling",
+	  "description": "Stirling Castle is one of Scotland's most historically important sites and was once a favoured residence of the Stewart kings and queens who held grand celebrations at the castle. Knights, nobles and foreign ambassadors once flocked to Stirling Castle to revel in its grandeur with its superb sculptures and beautiful gardens. It was a favoured residence of the Stewart kings and queens who held grand celebrations from christenings to coronations. Today you can meet the costumed characters in the roles of bodyguards, court officials, maids of honour and servants who will welcome you into 16th century life",
+	  "img": "/images/stirling_castle.jpg"
+	}
+	
+	]
+
 
 /***/ }
 /******/ ]);
--- client/src/app.js
+++ client/src/app.js
@@ -6,27 +6,57 @@ var historic = require('../api/historic.js');
 
 
 var app = function() {
-  populateList(whisky);
-  populateList(sports);
-  populateList(movies);
-  populateList(historic);
-}
+  console.log("here");
+  var whiskyButton = document.getElementById('distilleries');
+  var whiskyActive = false;
+  whiskyButton.onclick = function() {
+    if (whiskyActive) {
+      whiskyActive = false;
+      populateList(whisky,true);
+    } else {
+      whiskyActive = true;
+      populateList(whisky);
+    }
+  }
+};
 
-var populateList = function(thing) {
+  //   populateList(sports);
+  //   populateList(movies);
+  //   populateList(historic);
+  // }
+// var populateList = function(thing) {
+//   var list = document.getElementById('list');
+//   thing.forEach(function(item) {
+//     var liName = document.createElement('li');
+//     var liLocation = document.createElement('li');
+//     var liDescription = document.createElement('li');
+//     var liImg = document.createElement('img');
+//     liName.innerText = item.name;
+//     liLocation.innerText = item.location;
+//     liDescription.innerText = item.description;
+//     liImg.src = item.img;
+//     list.appendChild(liName);
+//     list.appendChild(liLocation);
+//     list.appendChild(liDescription);
+//     list.appendChild(liImg);
+//   })
+// };
+
+var populateList = function(thing,clear) {
   var list = document.getElementById('list');
+  list.innerHTML = "";
+  if (clear) return;
   thing.forEach(function(item) {
-    var liName = document.createElement('li');
-    var liLocation = document.createElement('li');
-    var liDescription = document.createElement('li');
-    var liImg = document.createElement('img');
-    liName.innerText = item.name;
-    liLocation.innerText = item.location;
-    liDescription.innerText = item.description;
-    liImg.src = item.img;
-    list.appendChild(liName);
-    list.appendChild(liLocation);
-    list.appendChild(liDescription);
-    list.appendChild(liImg);
+    var destination = document.createElement('li');
+    var checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    checkbox.name = item.name;
+    checkbox.value = false;
+    checkbox.id = item.name;
+
+    destination.innerText = item.name +', ' + item.location;
+    list.appendChild(destination);
+    list.appendChild(checkbox);
   })
 };
 
