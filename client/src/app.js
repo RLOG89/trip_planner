899af47f066191d08150db19751f45eb2d3b8f57
Thomas Crines <thomascrines@hotmail.com>
2016-12-15 15:20:15 +0000

    map and list combo

--- client/src/app.js
+++ client/src/app.js
@@ -7,7 +7,6 @@ var MapWrapper = require('./views/mapWrapper.js');
 
 
 var app = function() {
-<<<<<<< HEAD
   console.log("here");
   var whiskyButton = document.getElementById('distilleries');
   var whiskyActive = false;
@@ -20,76 +19,59 @@ var app = function() {
       populateList(whisky);
     }
   }
-};
 
-  //   populateList(sports);
-  //   populateList(movies);
-  //   populateList(historic);
-  // }
-// var populateList = function(thing) {
-//   var list = document.getElementById('list');
-//   thing.forEach(function(item) {
-//     var liName = document.createElement('li');
-//     var liLocation = document.createElement('li');
-//     var liDescription = document.createElement('li');
-//     var liImg = document.createElement('img');
-//     liName.innerText = item.name;
-//     liLocation.innerText = item.location;
-//     liDescription.innerText = item.description;
-//     liImg.src = item.img;
-//     list.appendChild(liName);
-//     list.appendChild(liLocation);
-//     list.appendChild(liDescription);
-//     list.appendChild(liImg);
-//   })
-// };
-=======
-  // populateList(whisky);
-  // populateList(sports);
-  // populateList(movies);
-  // populateList(historic);
 
   var mapDiv = document.getElementById('main-map');
-
   var startCoords = ({lat: 56.4907, lng: -4.2026});
-
   var newMap = new MapWrapper(mapDiv, startCoords, 6);
-};
->>>>>>> a68daaff00aa06d0ee5185ef3d32fb216b497856
 
-var populateList = function(thing,clear) {
-  var list = document.getElementById('list');
-  list.innerHTML = "";
-  if (clear) return;
-  thing.forEach(function(item) {
-<<<<<<< HEAD
-    var destination = document.createElement('li');
-    var checkbox = document.createElement('input');
-    checkbox.type = 'checkbox';
-    checkbox.name = item.name;
-    checkbox.value = false;
-    checkbox.id = item.name;
+  var populateList = function(thing,clear) {
+    var list = document.getElementById('list');
+    list.innerHTML = "";
+    if (clear) return;
+    thing.forEach(function(item) {
 
-    destination.innerText = item.name +', ' + item.location;
-    list.appendChild(destination);
-    list.appendChild(checkbox);
-  })
-=======
-    var liName = document.createElement('li');
-    var liLocation = document.createElement('li');
-    var liDescription = document.createElement('li');
-    var liImg = document.createElement('img');
-    liName.innerText = item.name;
-    liLocation.innerText = item.location;
-    liDescription.innerText = item.description;
-    liImg.src = item.img;
-    list.appendChild(liName);
-    list.appendChild(liLocation);
-    list.appendChild(liDescription);
-    list.appendChild(liImg);
-  });
->>>>>>> a68daaff00aa06d0ee5185ef3d32fb216b497856
+      var destination = document.createElement('li');
+      var checkbox = document.createElement('input');
+      checkbox.type = 'checkbox';
+      checkbox.name = item.name;
+      checkbox.value = false;
+      checkbox.id = item.name;
+
+      destination.innerText = item.name +', ' + item.location;
+      list.appendChild(destination);
+      list.appendChild(checkbox);
+    })
+  }
+
 };
 
 
 window.onload = app;
+
+    //   populateList(sports);
+    //   populateList(movies);
+    //   populateList(historic);
+    // }
+  // var populateList = function(thing) {
+  //   var list = document.getElementById('list');
+  //   thing.forEach(function(item) {
+  //     var liName = document.createElement('li');
+  //     var liLocation = document.createElement('li');
+  //     var liDescription = document.createElement('li');
+  //     var liImg = document.createElement('img');
+  //     liName.innerText = item.name;
+  //     liLocation.innerText = item.location;
+  //     liDescription.innerText = item.description;
+  //     liImg.src = item.img;
+  //     list.appendChild(liName);
+  //     list.appendChild(liLocation);
+  //     list.appendChild(liDescription);
+  //     list.appendChild(liImg);
+  //   })
+  // };
+
+    // populateList(whisky);
+    // populateList(sports);
+    // populateList(movies);
+    // populateList(historic);
