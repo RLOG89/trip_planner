7b502a48826ad9bebcf2ca714d385b46358ae094
Adrian Tuckwell <adrian.tuckwell@gmail.com>
2016-12-16 18:24:38 +0000

    added add trip method and test

--- client/src/organizer/specs/organizer_spec.js
+++ client/src/organizer/specs/organizer_spec.js
@@ -1,10 +1,16 @@
 var Organizer = require('../organizer');
-var Account = require('../trip');
+var Trip = require('../trip');
 var assert = require('assert');
 
 describe('organizer', function() {
   it('should start off with no bank trips ', function() {
     var organizer = new Organizer();
     assert.equal(0, organizer.trips.length);
+  });
+  it('should be able to add trip', function() {
+    var organizer = new Organizer();
+    var trip = new Trip({name:'Ross',budget:500});
+    organizer.addTrip(trip);
+    assert.deepEqual(trip, organizer.trips[0]);
   });
 });
