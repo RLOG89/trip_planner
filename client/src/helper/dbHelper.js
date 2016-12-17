2a2c4e5ed4009084d65d996208c9acbd65ede38e
Max <mveasey87@gmail.com>
2016-12-17 19:33:02 +0000

    added duration to post method in server.js, edited dbHelper.js, not yet working

--- client/src/helper/dbHelper.js
+++ client/src/helper/dbHelper.js
@@ -1,35 +1,61 @@
-use scotrip;
+var DbHelper = function() {
 
-db.trips.insert({
-  "name": "Trip to North",
-  "owner": "Ross",
-  "budget": 100,
-  "start_date": "12/12/16",
-  "end_date": "14/12/16",
-  "activities": []
-});
+}
 
-db.trips.update({name: 'Trip to North'},
-  {$push: {activities: {
-  "lat": 56.346025, 
-  "lng": -2.802696,
-  "sport": "Golf",
-  "location": "St. Andrews",
-  "name": "The Old Course",
-  "description": "The oldest and most iconic golf course in the world. The Swilcan Bridge and Hell Bunker are recognised across the globe, yet the greatest feature of the Old Course is that despite its grand status it remains a public golf course, open to all.",
-  "img": "/images/the_old_course.jpg"
-}}}
-);
+DbHelper.prototype = {
+  addItem: function() {
+    app.post('/trips', function(req,res) {
+      console.log('body', req.body);
+      MongoClient.connect(url, function(err, db) {
+        var collection = db.collection('trips');
+        collection.update({
+          {name: "Trip to North"}, 
+          {$push: {activities: {
+            "lat": 56.346025, 
+            "lng": -2.802696,
+            "sport": "Golf",
+            "location": "St. Andrews",
+            "name": "The Old Course",
+            "description": "The oldest and most iconic golf course in the world. The Swilcan Bridge and Hell Bunker are recognised across the globe, yet the greatest feature of the Old Course is that despite its grand status it remains a public golf course, open to all.",
+            "img": "/images/the_old_course.jpg"
+          }}}
+        );
+        res.status(200).end();
+        db.close();
+      });
 
-db.trips.update({name: 'Trip to North'},
-  {$pull: {activities: {
-  "name": "The Old Course",
-}}}
-);
+  
 
-db.trips.update({name: 'Trip to North'}, 
-  { $set : {activities: [] }
-});
+// db.trips.insert({
+//   "name": "Trip to North",
+//   "owner": "Ross",
+//   "budget": 100,
+//   "start_date": "12/12/16",
+//   "end_date": "14/12/16",
+//   "activities": []
+// });
+
+// db.trips.update({name: 'Trip to North'},
+//   {$push: {activities: {
+//   "lat": 56.346025, 
+//   "lng": -2.802696,
+//   "sport": "Golf",
+//   "location": "St. Andrews",
+//   "name": "The Old Course",
+//   "description": "The oldest and most iconic golf course in the world. The Swilcan Bridge and Hell Bunker are recognised across the globe, yet the greatest feature of the Old Course is that despite its grand status it remains a public golf course, open to all.",
+//   "img": "/images/the_old_course.jpg"
+// }}}
+// );
+
+// db.trips.update({name: 'Trip to North'},
+//   {$pull: {activities: {
+//   "name": "The Old Course",
+// }}}
+// );
+
+// db.trips.update({name: 'Trip to North'}, 
+//   { $set : {activities: [] }
+// });
 
 // db.animals.find({
 //   name: 'Fred'
