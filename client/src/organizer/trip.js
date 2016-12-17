e0142a6525e3085999d1ce96f2189dcd82854f49
Adrian Tuckwell <adrian.tuckwell@gmail.com>
2016-12-17 11:37:33 +0000

    added start and end date for trip

--- client/src/organizer/trip.js
+++ client/src/organizer/trip.js
@@ -2,6 +2,8 @@ var Trip = function(params) {
   this.name = params.name;
   this.owner = params.owner;
   this.budget = params.budget;
+  this.start_date = params.start_date;
+  this.end_date = params.end_date;
 };
 
 module.exports = Trip;
