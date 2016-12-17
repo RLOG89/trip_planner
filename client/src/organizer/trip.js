a2fc452577543da552a784aa67b61eb7313b42e3
Adrian Tuckwell <adrian.tuckwell@gmail.com>
2016-12-17 16:24:50 +0000

    added duration method

--- client/src/organizer/trip.js
+++ client/src/organizer/trip.js
@@ -4,6 +4,15 @@ var Trip = function(params) {
   this.budget = params.budget;
   this.start_date = params.start_date;
   this.end_date = params.end_date;
+  this.duration = 0;
 };
+
+Trip.prototype = {
+  getDuration: function() {
+    date1 = new Date(this.start_date);  
+    date2 = new Date(this.end_date); 
+    this.duration = Math.round((date2-date1)/(1000*60*60*24))
+  },
+}
 
 module.exports = Trip;
