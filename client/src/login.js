44ced516ebe4298a84a96f7f89152e28546d6734
RLOG89 <ross.loggie@gmail.com>
2016-12-19 18:54:54 +0000

    added password field and started validate function

--- client/src/login.js
+++ client/src/login.js
@@ -1,31 +1,7 @@
-var testLogin = [
-{
-  username: "Adrian",
-  password: "password"
-},
-{
- username: "Max",
- password: "12345" 
-},
-{
-  username: "Ross",
-  password: "testing" 
-},
-{
-  username: "Tom",
-  password: "TheFace" 
-}
-]
-
 validate = function() {
-  var username = document.getElementById("username").value
-  var password = document.getElementById("password").value
+  var username = document.getElementById("login-user-name").value
+  var password = document.getElementById("login-password").value
 
-  for (var i = 0; i < testLogin.length; i++) {
-    if (username == testLogin[i].username && password == testLogin[i].password) {
-      console.log(username + " is logged in!")
-      return;
-    }
-  }
-  console.log("incorrect username or password")
-}
+  ajaxHelper.makeGetRequest("http://localhost:3000/trips", username, function(password) {
+    if(password == )
+  })
