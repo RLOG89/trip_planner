1a61475e32a646b62d021297eccafa485e3e7177
Thomas Crines <thomascrines@hotmail.com>
2016-12-19 10:57:48 +0000

    add itinerary map

--- client/src/app.js
+++ client/src/app.js
@@ -125,6 +125,8 @@ var app = function() {
       newMap.addMarker(itemCoords, item.img, item.description);
     })
   };
+  var itineraryMapDiv = document.getElementById('itinerary-map');
+  var itineraryMap = MapWrapper(itineraryMapDiv, startCoords, 6);
 };
 
 window.onload = app;
