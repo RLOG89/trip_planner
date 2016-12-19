c3a2ea96f8ac99a4a84f9922b0e29e7d305dddc1
RLOG89 <ross.loggie@gmail.com>
2016-12-19 10:37:14 +0000

    added cost/duration to api

--- client/api/whisky.js
+++ client/api/whisky.js
@@ -6,7 +6,9 @@ module.exports = [
   "lat":          55.640752,
   "lng":          -6.108134,
   "description":  "Ardbeg Distillery is a Scotch whisky distillery in Ardbeg on the south coast of the isle of Islay, Argyll and Bute, Scotland, in the Inner Hebrides group of islands.",
-  "img":          "/images/ardbeg.png"
+  "img":          "/images/ardbeg.png",
+  "cost":         20,
+  "duration":     0.25 
 }, {
   "name":         "Balvenie",
   "location":     "Dufftown",
@@ -14,7 +16,9 @@ module.exports = [
   "lat":          57.458685,
   "lng":          -3.127234,
   "description":  "The Balvenie distillery is a Speyside single malt Scotch whisky distillery in Dufftown, Scotland, owned by William Grant & Sons.",
-  "img":          "/images/balvenie.png"
+  "img":          "/images/balvenie.png",
+  "cost":         10,
+  "duration":     0.25 
 }, {
   "name":         "Glenfarclas",
   "location":     "Ballindollach",
@@ -22,7 +26,9 @@ module.exports = [
   "lat":          57.427020,
   "lng":          -3.316349,
   "description":  "Glenfarclas distillery is a Speyside whisky distillery in Ballindalloch, Scotland. Glenfarclas translates as meaning valley of the green grass. The distillery is owned and run by the Grant family.",
-  "img":          "/images/glenfarclas.gif"
+  "img":          "/images/glenfarclas.gif",
+  "cost":         15,
+  "duration":     0.25 
 }, {
   "name":         "Glenfiddich",
   "location":     "Dufftown",
@@ -30,7 +36,9 @@ module.exports = [
   "lat":          57.454753,
   "lng":          -3.128639,
   "description":  "Glenfiddich is a Speyside single malt Scotch whisky owned and produced by William Grant & Sons in Dufftown, Scotland. Glenfiddich means ‘Valley of the Deer' in Scottish Gaelic, hence the presence of a stag symbol on Glenfiddich bottles.",
-  "img":          "/images/glenfiddich.png"
+  "img":          "/images/glenfiddich.png",
+  "cost":         25,
+  "duration":     0.25 
 }, {
   "name":         "Highland Park",
   "location":     "Kirkwall",
@@ -38,7 +46,9 @@ module.exports = [
   "lat":           58.967960,
   "lng":           -2.955335,
   "description":   "Highland Park distillery is a Scotch whisky distillery based in Kirkwall, Orkney. It is the most northerly whisky distillery in Scotland, half a mile farther north than that at Scapa distillery.",
-  "img":          "/images/highland_park.jpg"
+  "img":          "/images/highland_park.jpg",
+  "cost":         25,
+  "duration":     0.25 
 }, {
   "name":         "Laphroaig",
   "location":     "Port Ellen",
@@ -46,7 +56,9 @@ module.exports = [
   "lat":          55.630075,
   "lng":          -6.152225,
   "description":  "Laphroaig distillery is an Islay single malt Scotch whisky distillery. It is named for the area of land at the head of Loch Laphroaig on the south coast of the Isle of Islay.",
-  "img":          "/images/laphroaig.jpg"
+  "img":          "/images/laphroaig.jpg",
+  "cost":         15,
+  "duration":     0.25 
 }, {
   "name":         "The Macallan",
   "location":     "Craigellachie",
@@ -54,7 +66,9 @@ module.exports = [
   "lat":          57.484623,
   "lng":          -3.206944,
   "description":  "The Macallan distillery is a single malt Scotch whisky distillery in Craigellachie, Moray. The Macallan Distillers Ltd, is a wholly owned subsidiary of the Edrington Group which purchased the brand from Highland Distillers in 1999.",
-  "img":          "/images/macallan.jpeg"
+  "img":          "/images/macallan.jpeg",
+  "cost":         30,
+  "duration":     0.25 
 }, {
   "name":         "Pulteney",
   "location":     "Wick",
@@ -62,7 +76,9 @@ module.exports = [
   "lat":          58.435383,
   "lng":          -3.084711,
   "description":  "The Pulteney Distillery is a malt whisky production and aging facility in the Pulteneytown area of Wick, Caithness, in the Highland area of Scotland.",
-  "img":          "/images/pulteney.jpeg"
+  "img":          "/images/pulteney.jpeg",
+  "cost":         20,
+  "duration":     0.25 
 }, {
   "name":         "Springbank",
   "location":     "Mull of Kintyre",
@@ -70,7 +86,9 @@ module.exports = [
   "lat":          55.425747,
   "lng":          -5.608731,
   "description":  "Springbank distillery is one of the last surviving producers of Campbeltown Single Malts. The distillery, located on the southern Kintyre peninsula, produces three distinct types of single malt Scotch whisky.",
-  "img":          "/images/springbank.jpeg"
+  "img":          "/images/springbank.jpeg",
+  "cost":         20,
+  "duration":     0.25 
 }, {
   "name":         "Talisker",
   "location":     "Carbost, Isle of Skye",
@@ -78,6 +96,8 @@ module.exports = [
   "lat":          57.302313,
   "lng":          -6.356749,
   "description":  "Talisker distillery is an Island single malt Scotch whisky distillery based in Carbost, Scotland—the only distillery on the Isle of Skye.",
-  "img":          "/images/talisker.png" 
+  "img":          "/images/talisker.png" ,
+  "cost":         20,
+  "duration":     0.25 
 }
 ];
