159bae8ded83ab52bec0aeb9a79f9397280a8295
RLOG89 <ross.loggie@gmail.com>
2016-12-20 12:41:06 +0000

    added login functionality, can now login and go to itinerary page

--- client/src/login.js
+++
@@ -1,7 +0,0 @@
-validate = function() {
-  var username = document.getElementById("login-user-name").value
-  var password = document.getElementById("login-password").value
-
-  ajaxHelper.makeGetRequest("http://localhost:3000/trips", username, function(password) {
-    if(password == )
-  })
